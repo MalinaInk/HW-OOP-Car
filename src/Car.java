541b23c9061a1b3083cd61d660dd219ea02f218c
mo <nataleshen.ko@yandex.ru>
2022-10-25 22:52:16 +0300

    commit 2

---
+++ src/Car.java
@@ -0,0 +1,19 @@
+public class Car {
+    String brand;
+    String model;
+    double engineVolume;
+    String color;
+    int productionYear;
+    String productionCountry;
+
+
+       void getInfo() {
+        System.out.println(brand + model +
+                ", сбока в " + productionCountry +
+                ", в " + productionYear + " году, "
+                + color + " цвет кузова, " +
+                "объем - " + engineVolume + " л.");
+
+    }
+
+}
