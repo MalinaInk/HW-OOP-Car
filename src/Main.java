7a29933c0ec24cec3def7455399f9446443d80c9
mo <nataleshen.ko@yandex.ru>
2022-10-26 17:41:12 +0300

    final commit 4

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,19 @@ import java.util.concurrent.Callable;
 
 public class Main {
     public static void main(String[] args) {
-        Car car1 = new Car("Lada","Granta","России",2015,"желтый",1.7);
+        Car car1 = new Car("Lada","","России",2015,"желтый",1.7);
         car1.getInfo();
 
-        Car car2 = new Car("Audi", "A850 L TDI quattro","Германии",2020,"черный", 3.0);
+        Car car2 = new Car("Audi", "A850 L TDI quattro","",2020,"черный", 3.0);
         car2.getInfo();
 
-        Car car3 = new Car("BMW","Z8","Германии",2021,"черный",3.0);
+        Car car3 = new Car("BMW","Z8","Германии",2021,"",3.0);
         car3.getInfo();
 
-        Car car4 = new Car("Kia","Sportage 4-го поколения","Южной Корее",2018,"красный",2.4);
+        Car car4 = new Car("Kia","Sportage 4-го поколения","Южной Корее",0,"красный",2.4);
         car4.getInfo();
 
-        Car car5 = new Car("Hyundai","Avante","Южной Корее", 2016,"оранжевый",1.6);
+        Car car5 = new Car("Hyundai","Avante","Южной Корее", 2016,"оранжевый",0);
         car2.getInfo();
     }
 }
